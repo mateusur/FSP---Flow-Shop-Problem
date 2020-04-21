8d9e5240b3881fbd53209a3d9a5d9e9818e3fdd2
Mateusz Urbaniak <241614@student.pwr.edu.pl>
2020-04-21 22:59:16 +0200

    Working brute force

--- main.cpp
+++ main.cpp
@@ -12,51 +12,47 @@ struct task
 	unsigned p;
 	unsigned ID;
 };
-typedef vector<vector<task>> Smatrix;
-Smatrix load_data(unsigned nuber_of_file) {
+//typedef vector<vector<task>> matrix;
+matrix load_data(unsigned nuber_of_file) {
 	int tasks, machines, temp, temp2, id = 1;
-	Smatrix data;
+	matrix data;
 	//vector<unsigned> vtemp;
-	vector<task> vtemp;
+	vector<unsigned> vtemp;
 	std::ifstream file;
-	file.open("data/data002.txt", ios::out);
+	file.open("data/data005.txt", ios::out);
 	//file.open("data/" + names[nuber_of_file], std::ios::out);
 	file >> tasks >> machines;
 	for (int i = 0; i < tasks; i++) {
 		for (int j = 0; j < machines; j++) {
-			task temp3; 
+			//task temp3; 
 			file >> temp >> temp2;
-			temp3.p = temp2;
-			temp3.ID = j;
-			//vtemp.push_back(temp2);
-			vtemp.push_back(temp3);
+			//temp3.p = temp2;
+			/*temp3.ID = j;*/
+			vtemp.push_back(temp2);
+			//vtemp.push_back(temp3);
 		}
+		vtemp.push_back(i+1);
 		data.push_back(vtemp);
 		vtemp.clear();
 	}
 	//cout << dane.size();
 	for(int i=0;i<tasks;i++) {
-		for (int j = 0; j < machines;j++) {
-			cout << data[i][j].p << " " ;
+		for (int j = 0; j < machines+1;j++) {
+			cout << data[i][j] << " " ;
 		}
 		cout << endl;
 	}
-	for (int i = 0; i < tasks; i++) {
-		for (int j = 0; j < machines; j++) {
-			cout << data[i][j].ID << " ";
-		}
-		cout << endl;
-	}
+
 	//wypisanie podzadan dla zad 1
 	//for(int i=0;)
 	file.close();
 	return data;
 }
 
-int calculate_F(const Smatrix& data) {
+int calculate_F(const matrix& data) {
 	liczba_wywolan++;
 	unsigned tasks = data.size();
-	unsigned machines = data[1].size();
+	unsigned machines = data[1].size()-1;
 	//cout <<"Liczba zadan: "<< tasks << endl;
 	//cout << "Liczba maszyn: " << machines << endl;
 	vector<unsigned> temp(tasks, 0);
@@ -69,19 +65,19 @@ int calculate_F(const Smatrix& data) {
 			//wiersz
 			if (i == 0 && j == 0) {
 				S[i][j] = 0;
-				C[i][j] = data[j][i].p;
+				C[i][j] = data[j][i];
 			}
 			else if (i == 0) {
 				S[i][j] = C[i][j - 1];
-				C[i][j] = S[i][j] + data[j][i].p;
+				C[i][j] = S[i][j] + data[j][i];
 			}
 			else if (j == 0) {
 				S[i][j] = C[i - 1][j];
-				C[i][j] = S[i][j] + data[j][i].p;
+				C[i][j] = S[i][j] + data[j][i];
 			}
 			else {
 				S[i][j] = max(C[i][j - 1], C[i - 1][j]);
-				C[i][j] = S[i][j] + data[j][i].p;
+				C[i][j] = S[i][j] + data[j][i];
 			}
 		}
 	}
@@ -99,28 +95,28 @@ int calculate_F(const Smatrix& data) {
 	//	cout << endl;
 	//}
 	int Cmax = C[machines - 1][tasks - 1];
-	cout << Cmax << endl;
+	//cout << Cmax << endl;
 	return Cmax;
 }
-bool compareID(const task& t1, const task & t2) {
-	return t1.ID < t2.ID;
+bool compareID(const vector<unsigned> v1, vector<unsigned> v2) {
+	size_t size = v1.size()-1;
+	return v1[size] < v2[size];
 }
 
-auto brute_force_iteration(Smatrix data) {
+int brute_force_iteration(matrix data) {
 	int bestCmax = UINT16_MAX;
-	cout << "Rozmiar: " << data.size()<<endl;
-	//cout << data[0].size() << endl;
-	long long i = 0;
 	int temp = 0;
-	//do {
-	//	temp = calculate_F(data);
-	//	if (temp < bestCmax) bestCmax = temp;
-	//} while (next_permutation(data.begin(), data.end(),compareID));
+	do {
+		temp = calculate_F(data);
+		if (temp < bestCmax) bestCmax = temp;
+	} while (next_permutation(data.begin(),data.end(),compareID));
 	return bestCmax;
 }
+
+
 int bestCmax = INT16_MAX;
 int temp;
-void findPermutations(Smatrix str, int index, size_t n) {
+void findPermutations(matrix str, int index, size_t n) {
 	if (index >= n) {
 		temp = calculate_F(str);
 		if (temp < bestCmax) {
@@ -135,11 +131,11 @@ void findPermutations(Smatrix str, int index, size_t n) {
 	}
 }
 int main() {
-	Smatrix data = load_data(2);
+	matrix data = load_data(2);
 	//cout << calculate_F(data);
 	cout << brute_force_iteration(data)<<endl;
-	cout << "liczba wywolan: " << liczba_wywolan << endl;
-	/*findPermutations(data, 0, data.size());
-	cout << bestCmax << endl;*/
+	//cout << "liczba wywolan: " << liczba_wywolan << endl;
+	//findPermutations(data, 0, data.size());
+	//cout << bestCmax << endl;
 	return 0;
 }
